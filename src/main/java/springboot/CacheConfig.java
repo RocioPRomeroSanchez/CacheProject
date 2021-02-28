f2c5add38f2f38142533470fa124ba0b69deb3a7
RocioPaloma <rociopromero98@gmail.com>
2021-02-28 12:56:21 +0100

    commit constructor

--- src/main/java/springboot/CacheConfig.java
+++ src/main/java/springboot/CacheConfig.java
@@ -1,6 +1,15 @@
 package springboot;
 
 
+import jdk.nashorn.internal.objects.annotations.Constructor;
+import org.springframework.cache.CacheManager;
+
 public class CacheConfig {
     //TODO CAHECONFIG
+    CacheManager cacheManager;
+
+
+    public CacheConfig(CacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
 }
